d44bb378facc204de2c9a0a02dd03d1733850c65
Harshal Shah <harshal888shah@gmail.com>
2026-06-05 12:37:57 +0530

    Use environment variable for database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,8 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
